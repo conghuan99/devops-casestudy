743e308277400380c68d2a57d68403822d11e343
conghuan99 <conghuanbttbqn@gmail.com>
2026-05-24 13:13:17 +0000

    test 03

--- flask_app/app.py
+++ flask_app/app.py
@@ -12,7 +12,7 @@ def health():
 
 @app.route("/health01")
 def health01():
-    return jsonify({"status": "healthy 02"})
+    return jsonify({"status": "healthy 03"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
