8f30fb89a2710da5ee3e9be9006b6ccd2efb62bb
conghuan99 <conghuanbttbqn@gmail.com>
2026-05-25 15:58:22 +0000

    update flask app - add prometheus metric exporter

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify
+# Import thư viện exporter
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+
+#  Khởi tạo PrometheusMetrics và gắn nó vào app Flask
+# Tham số path='/metrics' sẽ tự động tạo ra một đường dẫn http://localhost:5000/metrics
+metrics = PrometheusMetrics(app, path='/metrics')
+
+# 3. Thêm một số thông tin tĩnh (Static Info) để hiển thị trên Dashboard cho đẹp
+metrics.info('app_info', 'Flask Application info', version='1.0.0')
 
 @app.route("/")
 def home():
@@ -10,9 +19,9 @@ def home():
 def health():
     return jsonify({"status": "healthy"})
 
-@app.route("/health01")
-def health01():
-    return jsonify({"status": "healthy 08"})
+# @app.route("/health01")
+# def health01():
+    # return jsonify({"status": "healthy 08"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
